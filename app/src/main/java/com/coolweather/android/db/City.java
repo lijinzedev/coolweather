c875c6dc42dbd226784109a0cf166b7ca362a13f
李金泽 <2533755010@qq.com>
2018-07-11 17:35:28 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,42 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int proviceid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProviceid() {
+        return proviceid;
+    }
+
+    public void setProviceid(int proviceid) {
+        this.proviceid = proviceid;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,42 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherid;
+    private int cityid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherid() {
+        return weatherid;
+    }
+
+    public void setWeatherid(String weatherid) {
+        this.weatherid = weatherid;
+    }
+
+    public int getCityid() {
+        return cityid;
+    }
+
+    public void setCityid(int cityid) {
+        this.cityid = cityid;
+    }
+}
